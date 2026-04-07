03f33e9d44007b385229a91b19d324c080fd8dcf
theresebraberg-arch <therese.braberg@gmail.com>
2026-04-07 12:53:02 +0200

    Add TBR books and board categories

---
+++ data.js
@@ -0,0 +1,38 @@
+const tbrBooks = [
+  "Powerless – Lauren Roberts",
+  "Fourth Wing – Rebecca Yarros",
+  "Iron Flame – Rebecca Yarros",
+  "The Housemaid – Freida McFadden",
+  "Never Lie – Freida McFadden",
+  "Things We Never Got Over – Lucy Score",
+  "Twisted Love – Ana Huang",
+  "Haunting Adeline – H.D. Carlton"
+];
+
+const board = [
+  "Free pick 🎁",
+  "Romantasy 💜",
+  "Back 1",
+  "TBR",
+  "Enemies to lovers 😏",
+  "Forward 2",
+  "Thriller 😱",
+  "Color 🎨",
+  "Fantasy 🐉",
+  "Back 3",
+  "Sports romance 🏒",
+  "TBR",
+  "5 star prediction ⭐",
+  "Forward 3",
+  "Stand alone 📖",
+  "TBR",
+  "Grumpy x sunshine ☀️",
+  "Spinner 🎡",
+  "Under 300 pages",
+  "500+ pages",
+  "New author",
+  "Series 📚",
+  "Cozy read ☕",
+  "TBR",
+  "Free pick 🎁"
+];
